c244f9cd76d40fddb152aac4e12d8851ec26c40a
YangYunfang <1464183562@qq.com>
2025-02-25 20:33:46 +0800

    原始的版本 LSTM 2024

--- Server_New.py
+++ Server_New.py
@@ -43,7 +43,7 @@ import time
 #
 #
 # def slope_function(x, y, data_list, terrain_kind):
-#     slope, intercept = np.polyfit(data_list[6:9, 0], data_list[:, 1], deg=1)
+#     slope, intercept = np.ployfit(data_list[6:9, 0], data_list[:, 1], deg=1)
 #     value = slope * x + intercept
 #     if terrain_kind > 0:
 #         if x < data_list[6, 0]:
@@ -63,58 +63,56 @@ import time
 
 
 def terrain_function(x, data_list, terrain_kind):
-    tterrain = 0
     if terrain_kind == 2:
-        # print(data_list[6:8, 0], data_list[6:8, 1])
-        slope, intercept = np.polyfit(data_list[6:9, 0], data_list[6:9, 1], deg=1)
+        slope, intercept = np.ployfit(data_list[6:9, 0], data_list[:, 1], deg=1)
         value = slope * x + intercept
         if x < data_list[6, 0]:
-            tterrain = 0
+            terrain = 0
         elif data_list[6, 0] <= x < data_list[10, 0]:
-            tterrain = value
+            terrain = value
         elif x >= data_list[10, 0]:
-            tterrain = data_list[10, 1]
+            terrain = data_list[10, 1]
     elif terrain_kind == -2:
-        slope, intercept = np.polyfit(data_list[7:9, 0], data_list[7:9, 1], deg=1)
+        slope, intercept = np.ployfit(data_list[6:9, 0], data_list[:, 1], deg=1)
         value = slope * x + intercept
         if x >= data_list[6, 0]:
-            tterrain = 0
+            terrain = 0
         elif data_list[10, 0] <= x < data_list[6, 0]:
-            tterrain = value
+            terrain = value
         elif x < data_list[10, 0]:
-            tterrain = data_list[10, 1]
+            terrain = data_list[10, 1]
     elif terrain_kind == 1:
         if x < data_list[6, 0]:
-            tterrain = 0
+            terrain = 0
         elif data_list[6, 0] <= x < data_list[7, 0]:
-            tterrain = data_list[6, 1]
+            terrain = data_list[6, 1]
         elif data_list[7, 0] <= x < data_list[8, 0]:
-            tterrain = data_list[7, 1]
+            terrain = data_list[7, 1]
         elif data_list[8, 0] <= x < data_list[9, 0]:
-            tterrain = data_list[8, 1]
+            terrain = data_list[8, 1]
         elif data_list[9, 0] <= x < data_list[10, 0]:
-            tterrain = data_list[9, 1]
+            terrain = data_list[9, 1]
         elif x >= data_list[10, 0]:
-            tterrain = data_list[10, 1]
+            terrain = data_list[10, 1]
     elif terrain_kind == -1:
         if x > data_list[6, 0]:
-            tterrain = 0
+            terrain = 0
         elif data_list[7, 0] <= x < data_list[6, 0]:
-            tterrain = data_list[6, 1]
+            terrain = data_list[6][1]
         elif data_list[8, 0] <= x < data_list[7, 0]:
-            tterrain = data_list[7, 1]
+            terrain = data_list[7, 1]
         elif data_list[9, 0] <= x < data_list[8, 0]:
-            tterrain = data_list[8, 1]
+            terrain = data_list[8, 1]
         elif data_list[10, 0] <= x < data_list[9, 0]:
-            tterrain = data_list[9, 1]
+            terrain = data_list[9, 1]
         elif x < data_list[10, 0]:
-            tterrain = data_list[10, 1]
+            terrain = data_list[10, 1]
     elif terrain_kind == 0:
-        tterrain = 0
-    return tterrain
+        terrain = 0
+    return terrain
 
 
-def handle_client(client_socket, other_device_socket, other_device_socket1):
+def handle_client(client_socket, other_device_socket):
     # global Toe_Info  # 脚尖信息z轴的位置和速度
     global com_Info
     global Knee_Info
@@ -130,7 +128,7 @@ def handle_client(client_socket, other_device_socket, other_device_socket1):
     try:
         while True:
             start_time = time.perf_counter()
-            data = client_socket.recv(150).decode('utf-8')
+            data = client_socket.recv(90).decode('utf-8')
             time2 = time.perf_counter()
             time_sec = time2 - time1
             # # 将字符串拆分为数组元素
@@ -147,23 +145,21 @@ def handle_client(client_socket, other_device_socket, other_device_socket1):
             # print(data_list)
             if not data:
                 break
-            if not data_list[4, 0] == 0 and origin == 0:
+            if number == 0:
                 origin = data_list[4, 0]
-                print("坐标系转换")
             if Terrain == -1 or Terrain == -2:
                 data_list[:, 0] = origin - data_list[:, 0]
             else:
                 data_list[:, 0] = data_list[:, 0] - origin
-            if not number:
-                data_list = data_list + 1
+
             if not np.any(data_list == 0) or not number:
                 # 除地形外其余特征
-                Features[0, 0] = (data_list[0, 0] + data_list[1, 0]) / 2  # 质心y位置
+                Features[0, 0] = (data_list[0, 0] + data_list[1, 0]) / 2000  # 质心y位置
                 com = (data_list[0, 1] + data_list[1, 1]) / 2  # 质心z原始高度
                 com_terrain = terrain_function(Features[0, 0], data_list, Terrain)  # 质心对应的地形
                 Features[0, 1] = (com - com_terrain) / Height  # 标准化后质心z位置
                 knee_terrain = terrain_function(data_list[2, 0], data_list, Terrain)  # 膝关节对应的地形
-                Features[0, 2:4] = [data_list[2, 0], (data_list[2, 1] - knee_terrain) / Height]
+                Features[0, 2:4] = [data_list[2, 0]/1000, (data_list[2, 1] - knee_terrain) / Height]
                 vel_com = np.array(([Features[0, 0], com] - com_Info) * 100)
                 Features[0, 4] = Features[0, 0] + vel_com[0, 0] * np.sqrt(Leg_Length / 9.8)
                 Features[0, 5] = Features[0, 1] + vel_com[0, 1] * np.sqrt(Leg_Length / 9.8) / Height
@@ -182,9 +178,7 @@ def handle_client(client_socket, other_device_socket, other_device_socket1):
                 angle_knee = math.degrees(knee_angle)
 
                 # 判断时相
-                # heel_height = data_list[4, 1] - terrain_function(data_list[3, 0] + 10, data_list, Terrain)
-                heel_height = data_list[4, 1] - terrain_function(data_list[4, 0], data_list, Terrain)  # slope
-                # heel_height = data_list[4, 1]
+                heel_height = data_list[4, 1] - terrain_function(data_list[4, 0] + 30, data_list, Terrain)
 
                 # 模型特征计算
                 if Terrain == 0:  # 水平行走
@@ -210,19 +204,18 @@ def handle_client(client_socket, other_device_socket, other_device_socket1):
 
                 send_str = ';'.join(['{:.2f}'.format(value) for value in data_array])
                 # send_str = ';'.join(['{:.2f}'.format(value) for value in send_data])
-                send_fix = send_str.ljust(25).encode('utf-8')
+                send_fix = send_str.ljust(18).encode('utf-8')
 
             # 发送处理后的数据到其他设备
             # sys.stdout.flush()  # 强制刷新缓冲区
 
             other_device_socket.send(send_fix)
-            other_device_socket1.send(send_fix)
             # start_time = time.perf_counter()
-            # save_list.append(np.array([leg_angle, pred_model, data_list[2, 0], data_list[2, 1], data_list[3, 0],
-            #                            data_list[3, 1], features[0, 0], features[0, 1], features[0, 2], features[0, 3],
-            #                            features[0, 4], features[0, 5], features[0, 6], features[0, 7]]))
-            save_list.append(np.array([angle_leg, time_sec, data_list[2, 0],
-                                       data_list[2, 1], data_list[3, 0], data_list[3, 1]]))
+            save_list.append(np.array([leg_angle, pred_model, data_list[2, 0], data_list[2, 1], data_list[3, 0],
+                                       data_list[3, 1], features[0, 0], features[0, 1], features[0, 2], features[0, 3],
+                                       features[0, 4], features[0, 5], features[0, 6], features[0, 7]]))
+            # save_list.append(np.array([angle_leg, time_sec, data_list[2, 0],
+            #                            data_list[2, 1], data_list[3, 0], data_list[3, 1]]))
             time1 = time2
             end_time = time.perf_counter()
             # print(start_time-end_time)
@@ -233,23 +226,23 @@ def handle_client(client_socket, other_device_socket, other_device_socket1):
         print("连接中断")
     finally:
         save_data = np.stack(save_list)
-        np.save('C:\\Users\\17543\\Desktop\\yangdata\\NPY1\\SlopeD_ZX_0128_3.npy', save_data)
+        np.save('C:\\Users\\17543\\Desktop\\yangdata\\NPY\\00123night_1.npy', save_data)
         client_socket.close()
 
 
 # 主函数
 # 个人参数的更改
-Mass = 78 * 5.7 / 100  # 体重/Kg, 男生5.7 女生：6.1，暂时不以百分数乘
+Mass = 75 * 5.7 / 100  # 体重/Kg, 男生5.7 女生：6.1，暂时不以百分数乘
 Height = 1820  # 身高/mm
-Leg_Length = 1.03  # 腿长/m
-Terrain = -2  # 地形限制, 0:平地，1:上楼梯，-1:下楼梯, 2：上斜坡， -2：下斜坡
+Leg_Length = 1.0  # 腿长/m
+Terrain = 1  # 地形限制, 0:平地，1:上楼梯，-1:下楼梯, 2：上斜坡， -2：下斜坡
 
 # 初始化
 Features = np.zeros((1, 8))  # 脚尖信息z轴的位置和速度
 # Toe_Info = np.zeros((2, 1))
 com_Info = np.zeros((1, 2))
 Knee_Info = np.zeros((2, 2))  # 速度和位置
-send_fix = ''.ljust(25).encode('utf-8')
+send_fix = ''.ljust(18).encode('utf-8')
 save_list = []
 
 # 停止进程
@@ -262,7 +255,7 @@ stop = 0
 # # sess = rt.InferenceSession("Lstm_Simplified.onnx")
 # input_name = sess.get_inputs()[0].name
 
-with h5py.File('LSTM_Params_V2.mat', 'r') as file:
+with h5py.File('LSTM_Params_V3.mat', 'r') as file:
     lstmInputWeights1 = np.array(file.get('lstmInputWeights1'))
     lstmRecurrentWeights1 = np.array(file.get('lstmRecurrentWeights1'))
     lstmBias1 = np.reshape(np.array(file.get('lstmBias1')), (400,))
@@ -295,8 +288,8 @@ model.compile(optimizer='adam', loss='mse')
 # 标准化参数
 muY = 14.9002
 sigmaY = 15.6916
-muX = np.array([[2.0123e+03, 0.5570, 2.0385e+03, 0.2833, 2.1391e+03, 0.5570, 5.6007e-05, 40.6117]])
-sigmaX = np.array([[1.3809e+03, 0.0248, 1.3863e+03, 0.0240, 1.3915e+03, 0.0383, 1.4690, 29.4007]])
+muX = np.array([[2.0123, 0.5570, 2.0385, 0.2833, 2.1391, 0.5570, 5.6007e-05, 40.6117]])
+sigmaX = np.array([[1.3809, 0.0248, 1.3863, 0.0240, 1.3915, 0.0383, 1.4690, 29.4007]])
 
 # 创建TCP套接字
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -311,16 +304,10 @@ server_socket.listen(1)
 print('等待连接...')
 
 # 连接到 "other device"，只打开一次
-other_device_address1 = ('192.168.1.108', 8910)
-other_device_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-other_device_socket1.connect(other_device_address1)
-print('连接EMG...')
-
-# 连接到 "other device"，只打开一次
 other_device_address = ('127.0.0.1', 5678)
 other_device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 other_device_socket.connect(other_device_address)
-print('连接Angle...')
+
 try:
     while True:
         # 接受连接
@@ -329,7 +316,7 @@ try:
 
         # 启动一个新线程来处理客户端连接
         client_handler = threading.Thread(target=handle_client,
-                                          args=(client_socket, other_device_socket, other_device_socket1))
+                                          args=(client_socket, other_device_socket))
         client_handler.start()
 except KeyboardInterrupt:
     print('服务中断')
@@ -337,4 +324,3 @@ finally:
     client_handler.join()
     server_socket.close()
     other_device_socket.close()
-    other_device_socket1.close()
